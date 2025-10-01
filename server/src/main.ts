6bbb8407aba719cf2bbb8815184526b9c796f3c6
DESKTOP-1658P1T\offset <minutd97@gmail.com>
2025-10-01 21:31:27 +0300

    Update 1.2.2 - server enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,32 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(
+//     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+//   );
+//   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
+// }
+
+// import { NestFactory } from '@nestjs/core'
+// import { AppModule } from './app.module'
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.enableCors({
+    origin: ['http://localhost:5173'], // your Vite dev origin
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    // credentials: false, // not using cookies
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
 
